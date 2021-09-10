b6489b89ce11653919ee1bb3196c396178feb532
Md. Sindabad <pavel.sindabad@gmail.com>
2021-09-10 14:50:01 +0600

    sass style-loader added

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-
+import './app.scss';
 const App = () => {
     return(
-        <h1>Hello from React!!!</h1>
+        <h1>Hello from <span>React</span>!!!</h1>
     )
 }
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
